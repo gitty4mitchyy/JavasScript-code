f192abe2b9adf4e6a65404ffda7d87598f3d3865
gitty4mitchyy <mitchellkesta@gmail.com>
2024-09-23 10:14:18 +0100

    Game created

---
+++ index.js
@@ -0,0 +1,18 @@
+// CODE TO CHECK THE NUMBER OF DAYS IN A MONTH
+
+// function check(params) {
+//     let inputValue = document.getElementById('input').value.toLowerCase().trim()
+
+//     if (inputValue === 'february' ) {
+//         alert(`${inputValue} has 29 days`)
+//     } else if (inputValue === 'january' || inputValue === "march" || inputValue === 'may' || inputValue === 'july' || inputValue === 'august' || inputValue === 'october' || inputValue === 'december'){
+//         alert(`${inputValue} has 31 days`)
+//     } else if(inputValue === 'april' || inputValue === 'september' || inputValue === 'june' || inputValue === 'november'){
+//         alert(`${inputValue} has 30 days`)
+//     } else if(inputValue === ''){
+//         alert('You didn\'t enter anything')
+//     }else{
+//         alert('Plaese, enter a valid month')
+//     }
+    
+// }
